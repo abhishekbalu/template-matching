5bd934fe14bef1cdedcc3337fd4d71c6dd3e16e7
abhishek balu <abhisheklokesh6008@gmail.com>
2018-10-21 02:29:03 +0530

    Initial commit

---
+++ CMakeFiles/work1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/work1.dir/work1.cpp.o"
+  "work1.pdb"
+  "work1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/work1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
